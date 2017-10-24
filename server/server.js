70ec2e7d5b20eff7ec378a09412ec00334b62c47
Elliscope <mifang@expedia.com>
2017-10-24 13:53:58 -0700

    clean up

--- server/server.js
+++ server/server.js
@@ -4,13 +4,14 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 
-app.use(express.static('nodejs'))
+app.use(express.static('expedia_files'))
 
 const PORT = 8080; 
 
 // app.listen(PORT, function () {
 //     	console.log('Example app listening on port 3000!')
 // })
+
 
 fs.readFile('../expedia.html', function (err, html) {
 
@@ -25,3 +26,18 @@ fs.readFile('../expedia.html', function (err, html) {
 
 });
 
+// var catalog = require('./routes/catalog'); //Import routes for "catalog" area of site
+// var compression = require('compression');
+
+// // Create the Express application object
+// var app = express();
+
+// ...
+
+// app.use(compression()); //Compress all routes
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', index);
+// app.use('/users', users);
+// app.use('/catalog', catalog); 
